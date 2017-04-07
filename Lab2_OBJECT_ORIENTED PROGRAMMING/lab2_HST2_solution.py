7fe2d19c667fe97ba9f18bf41c5aa407aa4544a9
Kevin Janvier <kevin.janvier5@gmail.com>
2017-04-07 21:51:05 +0300

    fix the hiden bug # solution  pass all the tests in lab2

--- Lab2_OBJECT_ORIENTED PROGRAMMING/lab2_HST2_solution.py
+++ Lab2_OBJECT_ORIENTED PROGRAMMING/lab2_HST2_solution.py
@@ -1,36 +1,32 @@
 class ShoppingCart(object):
     def __init__(self):
         self.total = 0
-        self.items = dict()
+        self.items = {}
         # Add an Item
-        
+
+
     def add_item(self, item_name, quantity, price):
-        if item_name and quantity and price:
-            if type(item_name) == str and type(quantity) == int and type(price) == int:
-                if item_name in self.items:
-                    self.items[item_name] += quantity
-                else:
-                    self.items[item_name] = quantity
-                self.total += quantity * price
-                
-                #Remove an Item 
+        item_cost = quantity * price
+        self.total += item_cost
+        self.items[item_name] = quantity
+                # Remove an Item
+
     def remove_item(self, item_name, quantity, price):
-        if item_name and quantity and price:
-            if type(item_name) == str and type(quantity) == int and type(price) == int:
-                if item_name in self.items:
-                    if quantity > self.items[item_name]:
-                        del self.items[item_name]
-                    else: 
-                        self.items[item_name] -= quantity
-                    self.total -= quantity * price
-                    
-                #   Checkout
+        if self.items[item_name] > quantity:
+            self.items[item_name] = self.items[item_name] - quantity
+            self.total -= quantity * price
+        else:
+            self.total -= price * self.items[item_name]
+            del self.items[item_name]
+                    #   Checkout
+
     def checkout(self, cash_paid):
-        if cash_paid and type(cash_paid) == int:    
-            if self.total > cash_paid:
-                return "Cash paid not enough"
+            if  cash_paid>= self.total:
+                balance = cash_paid - self.total
+                return balance
             else:
-                return cash_paid - self.total
+                return "Cash paid not enough"
+
 class Shop(ShoppingCart):
     def __init__(self):
         self.quantity = 100
